cdcebc6aa5ea8234436f03bcb1d458d2cc356b1e
saraosorio0510-cloud <saraosorio0510@gmail.com>
2026-05-13 08:32:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import funciones 
 opcion=0
-while opcion !="6": 
-    print("")
-    print("==========================================")
-    print("                   Menu                   ")
-    print("")
-    print("1. Registrar Datos")
-    print("2. Listar Datos")
-    print("3. Calcular Gastos Totales/por Categoria")
-    print("4. Generar Reporte")
-    print("5. Salir")
-    opcion= input("Ingrese una opcion")
-    match opcion:
-        case "1": funciones.registrar_gasto()
-        case "2": funciones.listargastos()
-        case "3": funciones.calcular_gastos()
-        case "4": funciones.reporte_gastos()
-        case "5": break
 
-        
+while opcion !="5": 
+        print("")
+        print("==========================================")
+        print("                   Menu                   ")
+        print("")
+        print("1. Registrar Datos")
+        print("2. Listar Datos")
+        print("3. Calcular Gastos Totales/por Categoria")
+        print("4. Generar Reporte")
+        print("5. Salir")
+        try:
+            opcion= input("Ingrese una opcion: ")
+            if opcion<"1" or opcion>"5":
+                 print("ERROR: opccion incorrecta")
+                 continue
+            match opcion:
+                case "1": funciones.registrar_gasto()
+                case "2": funciones.listargastos()
+                case "3": funciones.calcular_gastos()
+                case "4": funciones.reporte_gastos()
+                case "5": 
+                    print("Saliendo...") 
+                    break
+        except ValueError:
+             print("ERROR: Debe ingresar un numero")
